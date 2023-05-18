a192e36d8fe386401ececb34c40ac6d7b360876c
Gagan Mishra <gagan@minbox.in>
2023-05-18 11:38:20 -0700

    dummy code to read from file line by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let file = "/Users/gagan/dev/yatdb/testFile1.txt";
+    println!("Going to read from the file: {file}");
+
+    match read_lines(file) {
+        Ok(lines) => {
+            for line in lines {
+                if let Ok(l) = line {
+                    println!("{}", l);
+                }
+            }
+        },
+        Err(error) => panic!("Problem opening the file: {:?}", error)
+    };
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<File>>> where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
